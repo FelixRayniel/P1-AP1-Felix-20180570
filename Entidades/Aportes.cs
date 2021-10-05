13542f93e963ff5396af52255a475f8aa5c35a67
Felix <felix_reynoso1@ucne.edu.do>
2021-10-04 23:26:21 -0400

    Agregando la entidad principal aportes

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Felix_20180570.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteID { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Persona { get; set; }
+        public string Concepto { get; set; }
+        public double Monto { get; set; }
+    }
+}
