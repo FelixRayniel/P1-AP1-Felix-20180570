7eafe106efcf78f655d3c1c28a99db24361bbdaf
Felix <felix_reynoso1@ucne.edu.do>
2021-10-04 23:31:15 -0400

    Agregando el contexto al proyecto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Felix_20180570.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Felix_20180570.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@" Data Source = DATA\BDAportes.db");
+        }
+    }
+}
