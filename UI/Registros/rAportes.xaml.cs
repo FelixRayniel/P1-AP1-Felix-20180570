65bb6538096a60412e965810f0683964ccbe79b8
Felix <felix_reynoso1@ucne.edu.do>
2021-10-05 00:41:51 -0400

    Agregando la ventana de registro para el proyecto.

---
+++ UI/Registros/rAportes.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Felix_20180570.UI.Registros
+{
+    /// <summary>
+    /// Lógica de interacción para rAportes.xaml
+    /// </summary>
+    public partial class rAportes : Window
+    {
+        public rAportes()
+        {
+            InitializeComponent();
+        }
+    }
+}
