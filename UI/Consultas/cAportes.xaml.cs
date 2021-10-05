51e3959ad7a4b1da5c082ec8cb33553d993302b1
Felix <felix_reynoso1@ucne.edu.do>
2021-10-05 15:36:45 -0400

    Agregando la ventana de consulta al proyecto.

---
+++ UI/Consultas/cAportes.xaml.cs
@@ -0,0 +1,71 @@
+﻿using P1_AP1_Felix_20180570.BLL;
+using P1_AP1_Felix_20180570.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Felix_20180570.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cAportes.xaml
+    /// </summary>
+    public partial class cAportes : Window
+    {
+        public cAportes()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Aportes>();
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = AportesBLL.GetList(a => a.AporteID == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1:
+                        listado = AportesBLL.GetList(p => p.Persona.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = AportesBLL.GetList(c => true);
+            }
+
+            if (DesdeDataPicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date <= HastaDatePicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+            var conteo = listado.Count();
+            ConteoTextBox.Text = conteo.ToString();
+            
+
+            var monto = listado.Sum(x => x.Monto);
+            TotalTextBox.Text = monto.ToString();   
+            
+
+        }
+
+       
+    }
+}
